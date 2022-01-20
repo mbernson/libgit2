9f75ee5b0b1725e17843eb0d308ef7b42b85af91
Mathijs Bernson <mathijsb@q42.nl>
2022-01-20 15:56:19 +0100

    Initial support for Swift Package Manager

--- src/indexer.c
+++ src/indexer.c
@@ -28,7 +28,7 @@ size_t git_indexer__max_objects = UINT32_MAX;
 
 #define UINT31_MAX (0x7FFFFFFF)
 
-struct entry {
+struct _entry {
 	git_oid oid;
 	uint32_t crc;
 	uint32_t offset;
@@ -108,8 +108,8 @@ static int parse_header(struct git_pack_header *hdr, struct git_pack_file *pack)
 
 static int objects_cmp(const void *a, const void *b)
 {
-	const struct entry *entrya = a;
-	const struct entry *entryb = b;
+	const struct _entry *entrya = a;
+	const struct _entry *entryb = b;
 
 	return git_oid__cmp(&entrya->oid, &entryb->oid);
 }
@@ -421,7 +421,7 @@ static int store_object(git_indexer *idx)
 {
 	int i, error;
 	git_oid oid;
-	struct entry *entry;
+	struct _entry *entry;
 	off64_t entry_size;
 	struct git_pack_entry *pentry;
 	off64_t entry_start = idx->entry_start;
@@ -496,7 +496,7 @@ GIT_INLINE(bool) has_entry(git_indexer *idx, git_oid *id)
 	return git_oidmap_exists(idx->pack->idx_cache, id);
 }
 
-static int save_entry(git_indexer *idx, struct entry *entry, struct git_pack_entry *pentry, off64_t entry_start)
+static int save_entry(git_indexer *idx, struct _entry *entry, struct git_pack_entry *pentry, off64_t entry_start)
 {
 	int i;
 
@@ -530,7 +530,7 @@ static int hash_and_save(git_indexer *idx, git_rawobj *obj, off64_t entry_start)
 {
 	git_oid oid;
 	size_t entry_size;
-	struct entry *entry;
+	struct _entry *entry;
 	struct git_pack_entry *pentry = NULL;
 
 	entry = git__calloc(1, sizeof(*entry));
@@ -917,7 +917,7 @@ static int seek_back_trailer(git_indexer *idx)
 static int inject_object(git_indexer *idx, git_oid *id)
 {
 	git_odb_object *obj = NULL;
-	struct entry *entry = NULL;
+	struct _entry *entry = NULL;
 	struct git_pack_entry *pentry = NULL;
 	git_oid foo = {{0}};
 	unsigned char hdr[64];
@@ -1159,7 +1159,7 @@ int git_indexer_commit(git_indexer *idx, git_indexer_progress *stats)
 	int error;
 	struct git_pack_idx_header hdr;
 	git_str filename = GIT_STR_INIT;
-	struct entry *entry;
+	struct _entry *entry;
 	git_oid trailer_hash, file_hash;
 	git_filebuf index_file = {0};
 	void *packfile_trailer;
